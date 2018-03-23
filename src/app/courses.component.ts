b73f567882f988403104970f1f1dab56040afa93
Haris Begluk <haris.begluk.webprogramming@outlook.com>
2018-03-23 17:00:15 +0100

    Example of event filtering

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,28 +1,19 @@
 import { CoursesService } from './courses.services';
-//Importing Component Decorator
 import {Component} from '@angular/core' 
 
-//Component Decorator  
-//Event Binding 
-//<button (click)="onSave()" " >Save</button> 
-//Da bi bili u mogucnosti da manipulisemo sa event opcijama 
-//mozemo dodati $event property 
+
+
 @Component({
     selector: 'courses', 
     template: ` 
-    <div (click)="onDivClicked()">
-    <button (click)="onSave($event)">Save</button> 
-    </div>
+    <input (keyup)="onKeyUp($event)" />
     `  
 })
-//Courses Component
+
 export class CoursesComponent{   
-    //Primjer event bubble problema
-    onDivClicked($event){ 
-        alert("Div was clicked:  " + $event);
+    onKeyUp($event){
+        if($event.keyCode === 13) 
+        alert("You pressed Enter!");
     }
-   onSave($event){ 
-       $event.stopPropagation();//sprijecava event bubbling 
-       alert("Button was clicked:  " + $event);
-   }
+   
 }
