732e2b5db3aaf0fb5774303bb1790a6e11b2ef05
Haris Begluk <haris.begluk.webprogramming@outlook.com>
2018-03-23 17:03:46 +0100

    Example of event filtering with Angular syntax (corrected)

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -6,12 +6,12 @@ import {Component} from '@angular/core'
 @Component({
     selector: 'courses', 
     template: ` 
-    <input (keyup.enter)="onKeyUp($event)" />
+    <input (keyup.enter)="onKeyUp()" />
     `  
 })
 
 export class CoursesComponent{   
-    onKeyUp($event){
+    onKeyUp(){
        // if($event.keyCode === 13) 
         alert("You pressed Enter!");
     }
