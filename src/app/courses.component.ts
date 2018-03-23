4a7953bb212f06fa8af29c45455a56fb38db7021
Haris Begluk <haris.begluk.webprogramming@outlook.com>
2018-03-23 15:46:45 +0100

    Difference between interpolation and property binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,33 +2,25 @@ import { CoursesService } from './courses.services';
 //Importing Component Decorator
 import {Component} from '@angular/core' 
 
-//Component Decorator
+//Component Decorator 
+//<img src="{{ imageUrl }}"/>  Interpolation radi dobro za dodavanje  
+//dinamickih vrijednosti izmedju headings, div, paragraf ili 
+//bilo gdje zelimo rendati text  
+// <h1 [textContent]="title"></h1> ovaj primjer je komplikovan
+//<img [src]="title" /> Property Binding bolji za dodavanje vrijednosti 
+//atributima tagova
 @Component({
     selector: 'courses', 
-    template: `<h1>{{ "Title: " + getTitle() }}</h1> 
-     
-        <ul>
-        <li *ngFor="let course of courses"> 
-        {{ course }}
-        </li>
-        </ul>
-
+    template: `
+    <h1>{{ "Title: " + title }}</h1>  
+    <h1 [textContent]="title"></h1>
+    <img src="{{ imageUrl }}"/>  
+    <img [src]="imageUrl" />
     ` 
     
 })
 //Courses Component
 export class CoursesComponent{ 
     title = "List of Course";   
-    courses; 
-
-    constructor(service:CoursesService){ //Olaksava Unit Testing
-        //Problem sa ovom implementacijom  
-        //ako koristimo new operator tada smo usko vezani za CourseService 
-        //drugi problem je ako zelimo dodati novi parametar u course konstruktor 
-        //tada moramo dodati novi atribut npr.  let service = new CoursesService(1); 
-        //let service = new CoursesService(); Losa praksa
-        this.courses = service.getCourses();
-    }
-   
-
+    imageUrl = "https://www.w3schools.com/howto/img_fjords.jpg";
 }
