fbe9a1a893673e6672f3244877b59a74817fbd01
Haris Begluk <haris.begluk.webprogramming@outlook.com>
2018-03-23 14:20:12 +0100

    New component added trough angular CLI

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,11 +1,13 @@
-import {Component} from '@angular/core'
+//Importing Component Decorator
+import {Component} from '@angular/core' 
 
+//Component Decorator
 @Component({
     selector: 'courses', 
     template: "<h1>Courses</h1>"
     
     
 })
-
+//Courses Component
 export class CoursesComponent{
 }
