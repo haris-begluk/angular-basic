3927f4f9486311475d335bdfef86f16a7255ea7a
Haris Begluk <haris.begluk.webprogramming@outlook.com>
2018-03-23 16:41:49 +0100

    Example of Event binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,18 +3,18 @@ import { CoursesService } from './courses.services';
 import {Component} from '@angular/core' 
 
 //Component Decorator  
-//Style Binding 
-//<button [style.backgroundColor]="isActive ? 'blue' : 'red' " >Save</button>
+//Event Binding 
+//<button (click)="onSave()" " >Save</button>
 @Component({
     selector: 'courses', 
     template: `
-    <button [style.backgroundColor]="isActive ? 'blue' : 'red' " >Save</button>
+    <button (click)="onSave()">Save</button>
     ` 
     
 })
 //Courses Component
 export class CoursesComponent{ 
-   //isActive = false; //red  
-   isActive = true; //blue  
-  
+   onSave(){
+       alert("Welcome to Angular App");
+   }
 }
