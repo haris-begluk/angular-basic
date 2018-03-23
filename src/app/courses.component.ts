58593b07e66e105f876a5eb3f812a1e2f6841a6d
Haris Begluk <haris.begluk.webprogramming@outlook.com>
2018-03-23 16:00:08 +0100

    Adding new property to DOM with Angular syntax(propery binding)

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -8,19 +8,25 @@ import {Component} from '@angular/core'
 //bilo gdje zelimo rendati text  
 // <h1 [textContent]="title"></h1> ovaj primjer je komplikovan
 //<img [src]="title" /> Property Binding bolji za dodavanje vrijednosti 
-//atributima tagova
+//atributima tagova 
+//<td [colspan]="colSpan" ></td> td tag nema property u DOM modelu colspan 
+//zato cmo dobiti gresku u konzoli, ali ako zelimo dodati atribut (novi)
+//tada koristimo angular sintaksu : 
+//<td [attr.colspan]="colSpan" ></td>
 @Component({
     selector: 'courses', 
     template: `
-    <h1>{{ "Title: " + title }}</h1>  
-    <h1 [textContent]="title"></h1>
-    <img src="{{ imageUrl }}"/>  
-    <img [src]="imageUrl" />
+    <img [src]="imageUrl" /> 
+    <table>
+    <tr> 
+        <td [attr.colspan]="colSpan" ></td>
+    </tr>
+    </table>
     ` 
     
 })
 //Courses Component
 export class CoursesComponent{ 
-    title = "List of Course";   
-    imageUrl = "https://www.w3schools.com/howto/img_fjords.jpg";
+    imageUrl = ""; 
+    colSpan = 2;
 }
