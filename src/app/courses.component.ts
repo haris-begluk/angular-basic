128b9c2689a5d3959336f8370c1136148e01aa9f
Haris Begluk <haris.begluk.webprogramming@outlook.com>
2018-03-23 14:46:31 +0100

    Example of Angular directives

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,15 +4,20 @@ import {Component} from '@angular/core'
 //Component Decorator
 @Component({
     selector: 'courses', 
-    template: '<h1>{{ "Title: " + getTitle() }}</h1>'  
+    template: `<h1>{{ "Title: " + getTitle() }}</h1> 
+     
+        <ul>
+        <li *ngFor="let course of courses"> 
+        {{ course }}
+        </li>
+        </ul>
+
+    ` 
     //This special syntax is caled "String Interpolation"
     //template: '<h1>{{ "Title: " + title }}</h1>' 
 })
 //Courses Component
 export class CoursesComponent{ 
-    title = "List of Course"; 
-
-    getTitle(){
-        return this.title;
-    }
+    title = "List of Course";  
+    courses =  ["Course One" , "Course Two", "Course Three"];
 }
