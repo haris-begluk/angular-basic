843fc9d341daa354c30e6c5c9926c3ed8378a28b
Haris Begluk <haris.begluk.webprogramming@outlook.com>
2018-03-23 18:35:58 +0100

    Example of template variables

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -6,14 +6,14 @@ import {Component} from '@angular/core'
 @Component({
     selector: 'courses', 
     template: ` 
-    <input (keyup.enter)="onKeyUp()" />
+    <input (keyup.enter)="onKeyUp($event)" />
     `  
 })
 
 export class CoursesComponent{   
-    onKeyUp(){
+    onKeyUp($event){
        // if($event.keyCode === 13) 
-        alert("You pressed Enter!");
+        alert("You pressed Enter!" + $event.target.value);
     }
    
 }
