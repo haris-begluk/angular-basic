b4a9b689e5f76f7b58f82abdf8ed8fcc42318881
Haris Begluk <haris.begluk.webprogramming@outlook.com>
2018-03-23 16:51:20 +0100

    Example of Event bubbling

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,17 +4,24 @@ import {Component} from '@angular/core'
 
 //Component Decorator  
 //Event Binding 
-//<button (click)="onSave()" " >Save</button>
+//<button (click)="onSave()" " >Save</button> 
+//Da bi bili u mogucnosti da manipulisemo sa event opcijama 
+//mozemo dodati $event property 
 @Component({
     selector: 'courses', 
-    template: `
-    <button (click)="onSave()">Save</button>
-    ` 
-    
+    template: ` 
+    <div (click)="onDivClicked()">
+    <button (click)="onSave($event)">Save</button> 
+    </div>
+    `  
 })
 //Courses Component
-export class CoursesComponent{ 
-   onSave(){
-       alert("Welcome to Angular App");
+export class CoursesComponent{   
+    //Primjer event bubble problema
+    onDivClicked($event){
+        alert("Div was clicked:  " + $event);
+    }
+   onSave($event){
+       alert("Button was clicked:  " + $event);
    }
 }
