8345b4c6b7f1f79f38eb159d266049fc553cde88
Haris Begluk <haris.begluk.webprogramming@outlook.com>
2018-03-24 18:09:36 +0100

    Directives ngIf old syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FavoriteChangedEventArgs } from './favorite/favorite.component';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +6,6 @@ import { FavoriteChangedEventArgs } from './favorite/favorite.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- post = {
-   title: "Title", 
-   isFavorite: true 
-   
- } 
+ courses = []; 
  
- onFavoriteChanged(eventArgs:FavoriteChangedEventArgs){
-   alert("Changed " + eventArgs.newValue );
- }
 }
