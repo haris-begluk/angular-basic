b977a3c16275539188b10a688379fafbbf453095
Haris Begluk <haris.begluk.webprogramming@outlook.com>
2018-03-24 14:27:12 +0100

    Component API Input properties second way

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.css'], 
-  //inputs:['isFavorite']
+  styleUrls: ['./favorite.component.css'],  
+  //Binding property
+  inputs:['isFavorite'] //drugi nacin za binding properties 
+  //Ali prblem sa ovim nacinom je magicni string
 })
 export class FavoriteComponent implements OnInit {
-  @Input() isFavorite:boolean;
+  isFavorite:boolean;
   constructor() { }
 
   ngOnInit() {
