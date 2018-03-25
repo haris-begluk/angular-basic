0331f09ecee87156fdc77cabe4b799f8bea39000
Haris Begluk <haris.begluk.webprogramming@outlook.com>
2018-03-25 21:22:04 +0200

    The leading asterisk for directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- courses;
-
- loadCourses(){
-  this.courses = [ 
+  courses = [ 
     { id:1 , name:'course1'}, 
     { id:2 , name:'course2'}, 
     { id:3 , name:'course3'}
   
   ]; 
- } 
-//Na ovaj nacin mijenjamo kako ce Angular pratit objekte 
-//Umjesto da ih prati na osnovu identiteta objekta 
-//sada ce ih pratiti na osnovi id-a objekta
-    trackCourse(index, course){
-      return course ? course.id : undefined;
-    }
+
+ 
  }
