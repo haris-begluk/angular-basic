2cb69ded9d22dc4dd04a1425dfa8d4d58df9813b
Haris Begluk <haris.begluk.webprogramming@outlook.com>
2018-03-25 21:04:08 +0200

    Directives ngFor and change detection loading problem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- courses = [ 
+ courses;
+
+ loadCourses(){
+  this.courses = [ 
     { id:1 , name:'course1'}, 
     { id:2 , name:'course2'}, 
     { id:3 , name:'course3'}
   
   ]; 
-
-  onAdd(){
-    this.courses.push({id:4, name:'course 4'});
-  } 
-  onChange(course){
-   course.name ="Updated";
-  }
+ }
  }
