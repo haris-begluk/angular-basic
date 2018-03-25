c132ef3b018b2ebcb1d8cc9c0649e18f918f6d84
Haris Begluk <haris.begluk.webprogramming@outlook.com>
2018-03-25 20:59:31 +0200

    Directives ngFor and change detection update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,8 +16,7 @@ export class AppComponent {
   onAdd(){
     this.courses.push({id:4, name:'course 4'});
   } 
-  onRemove(course){
-    let index = this.courses.indexOf(course); 
-    this.courses.splice(index, 1);
+  onChange(course){
+   course.name ="Updated";
   }
  }
