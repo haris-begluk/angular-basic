32fbd28b69dc50e8e71f2fb01efc55245244efbd
Haris Begluk <haris.begluk.webprogramming@outlook.com>
2018-03-26 11:05:48 +0200

    Reactive Forms Implementing Asynchronous Validation

--- src/app/signup-form/username.validators.ts
+++ src/app/signup-form/username.validators.ts
@@ -1,11 +1,29 @@
 import { AbstractControl, ValidationErrors } from "@angular/forms";
 
-
+//Ovdje cemo simulirati poziv na server  
+//Pozive na server zovemo Asynchronous Operations 
+//Kad zelimo provjeriti validaciju preko servera: 
+//Pozivanje servera koristeci AJAX 
+//Timer functions 
 export class UsernameValidators{
   static cannotContainSpace(control:AbstractControl):ValidationErrors | null{
     if((control.value as string).indexOf(' ')>=0) 
     return { cannotContainSpace:true };
     // return { minlength:{ requiredLength:10 , actualLength: control.value.length } } 
     return null;
+    } 
+
+    static shouldBeUniq(control: AbstractControl): ValidationErrors | null { 
+        //Timer function koja ce ispisati u konzoli 
+        //nakon 2000 ms, takodjer primjer anonymous function
+        setTimeout(() => { //Nece raditi kao sto bi trebalo 
+            console.log('ok'); 
+            if(control.value === 'Mosh') 
+        return { shouldBeUniq: true };
+        return null;
+        }, 2000); 
+
+        return null;//ovo nije dobro jer ce uvijek vratiti null sto znaci da 
+        //nasa funkcija nece prijaviti gresku
     }
 }
