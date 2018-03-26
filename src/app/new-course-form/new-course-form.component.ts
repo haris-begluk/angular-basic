625bd54f65a7ca9446d7f69269bf38340e44efbb
Haris Begluk <haris.begluk.webprogramming@outlook.com>
2018-03-26 22:45:12 +0200

    Using FormBuilder class correction

--- src/app/new-course-form/new-course-form.component.ts
+++ src/app/new-course-form/new-course-form.component.ts
@@ -28,6 +28,18 @@ constructor(fb: FormBuilder){
       phone:[]
     }), 
     topics:fb.array([])
-  });
+  }); 
+}  
+addTopic(topic: HTMLInputElement){
+  this.topics.push(new FormControl(topic.value)); 
+   topic.value = ''; 
+}   
+
+removeTopic(topic: FormControl){
+     let index = this.topics.controls.indexOf(topic); 
+     this.topics.removeAt(index);
+}
+get topics(){
+  return this.form.get('topics') as FormArray;
 }
 }
