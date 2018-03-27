2ebb94603337615fc04f7a2b5d7ad1275ef4f772
Haris Begluk <haris.begluk.webprogramming@outlook.com>
2018-03-27 12:16:29 +0200

    Consuming HTTP services Handling Unexpected Errors

--- src/app/Services/post.service.ts
+++ src/app/Services/post.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class PostService {
   private url = 'http://jsonplaceholder.typicode.com/posts'; 
-  
+
   constructor(private http: Http) {
 
    } 
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -22,6 +22,9 @@ export class PostsComponent implements OnInit {
         .subscribe(response => {
               //console.log(response.json()); 
               this.posts = response.json();
+        },error =>{
+          alert('Unexpected error occurred.'); 
+          console.log(error);
         }); 
     }
 
@@ -35,6 +38,9 @@ export class PostsComponent implements OnInit {
           post['id'] = response.json().id; 
           this.posts.splice(0,0,post);
           console.log(response.json()); //da vidimo objekat u konzoli
+        }, error =>{
+          alert('Unexpected error occurred.'); 
+          console.log(error);
         });
     } 
 
@@ -42,7 +48,10 @@ export class PostsComponent implements OnInit {
      this.service.updatePost(post)
       .subscribe(response =>{
         console.log(response.json());
-      })
+      }, error =>{
+        alert('Unexpected error occurred.'); 
+        console.log(error);
+      });
       // this.http.put(this.url,JSON.stringify(post));
     }
 
@@ -51,6 +60,9 @@ export class PostsComponent implements OnInit {
       .subscribe(respnse =>{
           let index = this.posts.indexOf(post); 
           this.posts.splice(index,1);
+      },error =>{
+        alert('Unexpected error occurred.'); 
+        console.log(error);
       });
     }
 }
