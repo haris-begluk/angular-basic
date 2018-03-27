b40f26453c6acf634e03f3a71ef788de9ae832e6
Haris Begluk <haris.begluk.webprogramming@outlook.com>
2018-03-27 10:36:45 +0200

    Consumint HTTP services post data to server api

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -12,12 +12,27 @@ import { Http } from '@angular/http';
 export class PostsComponent  { 
   //primjer kako da preuzmemo podatke sa servera (API) 
   //pretvorimo ih u json objekte te prikazemo na stranici
-    posts: any[];
-    constructor(http: Http){
-        http.get('http://jsonplaceholder.typicode.com/posts') 
+    posts: any[]; 
+    private url = 'http://jsonplaceholder.typicode.com/posts';
+    constructor(private http: Http){
+        http.get(this.url) 
         .subscribe(response => {
               //console.log(response.json()); 
               this.posts = response.json();
+        });
+    } 
+
+
+    createPost(input:HTMLInputElement){ 
+      let post ={
+        title: input.value
+      }; 
+      input.value = "";
+        this.http.post(this.url,JSON.stringify(post)) 
+        .subscribe(response => { 
+          post['id'] = response.json().id; 
+          this.posts.splice(0,0,post);
+          console.log(response.json()) //da vidimo objekat u konzoli
         });
     }
 }
