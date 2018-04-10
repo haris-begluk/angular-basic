2781fe700efca0428323987d60186aaedeed3d8a
Haris Begluk <haris.begluk.webprogramming@outlook.com>
2018-04-10 15:51:15 +0200

    Programmatic Navigation

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -1,33 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-github-profile',
   templateUrl: './github-profile.component.html',
   styleUrls: ['./github-profile.component.css']
 })
-export class GithubProfileComponent implements OnInit {
+export class GithubProfileComponent {
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private router:Router) { }
 
-  ngOnInit() { 
-   // console.log("Ovo je poziv ngOnInit funkcije!"); 
-   //a ovom funkcijom smo provjeravali kako se pozivaju nove stranice 
-   //Prilikom navigacije angular unisti stari page nakon toga kreira novi i inicijalizira ga 
-   //Ovaj nacin pristupa parametrima koristimo kada zelimo da 
-   //korisnik ostane na istoj stranici ali da se navigira naprijed i nazad kroz npr. korisnike ili slicno 
-   //npr. kada otvorimo bankovni racun i navigiramo se kroz transakcije  
-   //Ako zelimo da korisnik moze da ode negdje drugo sa jedne stranice i vrati se ponovo a da se stvari ne 
-   //mijenjaju mozemo koristiti snapshot 
-
-
-   // let id = this.route.snapshot.paramMap.get('id'); 
-    //console.log(id); 
-
-   this.route.paramMap.subscribe(params =>{
-     console.log(params.get('id'));
-   });
-    }
+ submit(){
+   this.router.navigate(['/followers'], {
+      queryParams: {
+        page:1, 
+        order: 'newest'
+      }
+   })
+ }
   }
 
 
