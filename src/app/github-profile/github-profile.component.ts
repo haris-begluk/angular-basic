d5129435a2093f890c2b8bb638b98db3371135d8
Haris Begluk <haris.begluk.webprogramming@outlook.com>
2018-04-10 15:04:38 +0200

    Why Route Parameters Are Observables?

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -11,12 +11,23 @@ export class GithubProfileComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() { 
-    this.route.paramMap 
-    .subscribe(params => {
-       // console.log(params); 
-      let id = +params.get('id'); 
-      console.log(id);
-    });
+   // console.log("Ovo je poziv ngOnInit funkcije!"); 
+   //a ovom funkcijom smo provjeravali kako se pozivaju nove stranice 
+   //Prilikom navigacije angular unisti stari page nakon toga kreira novi i inicijalizira ga 
+   //Ovaj nacin pristupa parametrima koristimo kada zelimo da 
+   //korisnik ostane na istoj stranici ali da se navigira naprijed i nazad kroz npr. korisnike ili slicno 
+   //npr. kada otvorimo bankovni racun i navigiramo se kroz transakcije  
+   //Ako zelimo da korisnik moze da ode negdje drugo sa jedne stranice i vrati se ponovo a da se stvari ne 
+   //mijenjaju mozemo koristiti snapshot 
+
+
+   // let id = this.route.snapshot.paramMap.get('id'); 
+    //console.log(id); 
+
+   this.route.paramMap.subscribe(params =>{
+     console.log(params.get('id'));
+   });
+    }
   }
 
-}
+
