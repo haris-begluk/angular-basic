802b579419a68dac0cf88ce88b921d9ae860f74a
haris-begluk <harisbegluk@gmail.com>
2018-04-13 18:02:07 +0200

    Protecting Routes Based on the User Role

---
+++ src/app/services/admin-auth-guard.service.ts
@@ -0,0 +1,20 @@
+import { CanActivate, Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AdminAuthGuard implements CanActivate {
+
+  constructor(private router: Router, 
+  private authService:AuthService ) { } 
+
+  canActivate(){ 
+    let user = this.authService.currentUser;
+     if(user && user.admin) {
+       return true;
+     } 
+     this.router.navigate(['/no-access']); 
+     return false;
+  }
+
+}
